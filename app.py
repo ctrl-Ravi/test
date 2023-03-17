35d99ec9043f7ab88385330b30ef67482fc9cf2a
Ravi Prakash <72187399+Ravi24242424@users.noreply.github.com>
2023-03-17 14:31:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ app = Flask(__name__)
 @app.route("/", methods = ['GET'])
 @cross_origin()
 def homepage():
-    return render_template("index.html")
+    return render_template("templates/index.html")
 
 @app.route("/review" , methods = ['POST' , 'GET'])
 @cross_origin()
